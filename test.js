c9c75197ec663ae6df88c38cc33c806865f1adef
Casterbillabong <casterbillabong@gmail.com>
2020-02-18 23:37:50 +0300

    youyou

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
-var http = require('http');
-const PORT = process.env.PORT ||8080;
-var server = http.createServer(function(req, res) {
-res.writeHead(200, {"Content-Type": "text/html"});
-res.end('<p>Voici un paragraphe <strong>HTML</strong> !</p>');
+var express = require('express')
+var app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/',function(req, res){
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil');
 });
-server.listen(8080);
+app.listen(PORT)
